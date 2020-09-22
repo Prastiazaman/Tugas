73e75fd181079886fd909803827490eed21ab1a8
Prastia Zaman <prastiazaman524@gmail.com>
2020-09-22 11:40:40 +0700

    Soal2.java

---
+++ Tugas1/Soal2.java
@@ -0,0 +1,9 @@
+public class Soal2{
+ public static void main(String[]args){
+  String name = "Implementasi Algoritma.";
+  int jumlah_jam = 3;
+  System.out.println("Aku suka kuliah "+ name);
+  System.out.println("Kuliah "+ jumlah_jam +"jam per minggu rasanya kurang.");
+  System.out.println("Tambahin dong jadi "+ (3 * jumlah_jam) + "jam per minggu :)");
+ }
+}
